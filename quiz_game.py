43e16ef02e04f26b661255f9e06ece4910991315
Tabassum <tabassumabedi2024@gmail.com>
2025-04-18 12:12:51 +0100

    changed score per question

--- quiz_game.py
+++ quiz_game.py
@@ -12,35 +12,35 @@ score = 0
 answer = input("What does CPU stand for? ")
 if answer.lower() == "central processing unit":
     print("Correct!✅")
-    score += 1 #increment score if answer is correct
+    score += 2 #increment score if answer is correct
 else:
     print("Incorrect❌")
 
 answer = input("What does HTML stand for? ")
 if answer.lower() == "hypertext markup language":
     print("Correct!✅")
-    score += 1
+    score += 2
 else:
     print("Incorrect❌")
 
 answer = input("What does RAM stand for ")
 if answer.lower() == "random access memory":
     print("Correct!✅")
-    score += 1
+    score += 2
 else:
     print("Incorrect❌")
 
 answer = input("What does URL stand for? ")
 if answer.lower() == "uniform resource locator":
     print("Correct!✅")
-    score += 1
+    score += 2
 else:
     print("Incorrect❌")
 
 answer = input("What does SQL stand for? ")
 if answer.lower() == "structured query language":
     print("Correct!✅")
-    score += 1
+    score += 2
 else:
     print("Incorrect❌")
 
