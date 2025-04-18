22e231f92884ff5b3ee264289a2721f96c33229c
Tabassum <tabassumabedi2024@gmail.com>
2025-04-18 12:11:27 +0100

    Built Acronym Quiz in Python with 5 questions and dynamic score tracking

---
+++ quiz_game.py
@@ -0,0 +1,49 @@
+print("Welcome to my Acronym Quiz")
+
+playing = input("Do you want to play?")
+
+
+if playing.lower() != "yes":
+    quit() #the program will quit if they type anything other than yes
+
+print("Okay! Let's play😄")
+score = 0
+
+answer = input("What does CPU stand for? ")
+if answer.lower() == "central processing unit":
+    print("Correct!✅")
+    score += 1 #increment score if answer is correct
+else:
+    print("Incorrect❌")
+
+answer = input("What does HTML stand for? ")
+if answer.lower() == "hypertext markup language":
+    print("Correct!✅")
+    score += 1
+else:
+    print("Incorrect❌")
+
+answer = input("What does RAM stand for ")
+if answer.lower() == "random access memory":
+    print("Correct!✅")
+    score += 1
+else:
+    print("Incorrect❌")
+
+answer = input("What does URL stand for? ")
+if answer.lower() == "uniform resource locator":
+    print("Correct!✅")
+    score += 1
+else:
+    print("Incorrect❌")
+
+answer = input("What does SQL stand for? ")
+if answer.lower() == "structured query language":
+    print("Correct!✅")
+    score += 1
+else:
+    print("Incorrect❌")
+
+
+print("You got " + str(score) + " questions correct!") #covert score to string
+print("You got " + str((score/5) * 100) + "%.")
