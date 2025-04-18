528949ab9c0f631ce6689f6fb88a5cce3fbdd6bf
Tabassum <tabassumabedi2024@gmail.com>
2025-04-18 12:45:02 +0100

    feat: implement number guessing game with random number generation and input validation

--- Guess_number.py
+++ Guess_number.py
@@ -1,9 +1,39 @@
 #working with the random module
 import random
 
-#lets make a random number
-print(random.randrange(11))#this will start from 0 automatocally up to 11(11 is exclusive)
-print(random.randrange(-2, 13))#start at -2 and stop at 13(13 is not inclusive), generate a random number in between
+#ask the user how large of a number to generate
 
-#now lets use randint this will include the  upper bound
-print(random.randint(1,8)) # 8 is inclusive now
+top_of_range = input("Type a number: ")
+
+#test what they type is a number and greater than 0
+if top_of_range.isdigit():
+    top_of_range = int(top_of_range) #convert the string input to integer
+
+    if top_of_range <= 0:
+        print("Please enter a number larger than 0 next time.")
+        quit()
+else:
+    print("Please type a number next time!")
+    quit()
+random_number = random.randint(0, top_of_range)
+
+#keep asking the user to guess the number until they get it currect
+guesses = 0
+
+while True:
+    guesses += 1 #increment guesses with more tries
+    user_NumberGuess = input("Make a guess: ")
+    if user_NumberGuess.isdigit():
+        user_NumberGuess = int(user_NumberGuess)
+    else:
+        print("Please type a number next time!")
+        continue #go back to the top of the while loop
+
+    if user_NumberGuess == random_number:
+        print("You have guessed the currect number!✅😄")
+        break #stop the loop
+    else:
+        print("Ops, that is wrong!")
+
+print("You guessed the number in", guesses, "guesses")
+
